8bd2a8616b794306710389a8c407df4689cc7141
viktorprogger <viktorprogger@gmail.com>
2019-05-27 15:55:35 +0300

    Return Psalm and fix an typehint issue

--- src/Exceptions/WrongAmountException.php
+++ src/Exceptions/WrongAmountException.php
@@ -3,10 +3,12 @@
 namespace miolae\Accounting\Exceptions;
 
 use miolae\Accounting\Interfaces\ExceptionInterface;
+use RuntimeException;
+use Throwable;
 
-class WrongAmountException extends \RuntimeException implements ExceptionInterface
+class WrongAmountException extends RuntimeException implements ExceptionInterface
 {
-    public function __construct($amount, int $code = 0, \Throwable $previous = null)
+    public function __construct(float $amount, int $code = 0, Throwable $previous = null)
     {
         $message = sprintf('Funds amount must be a positive number, "%s" given', $amount);
         parent::__construct($message, $code, $previous);
